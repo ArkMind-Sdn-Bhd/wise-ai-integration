535410254cc184dbaa333a0d13c6f22790b65db6
Draden Liang Han Sheng <hanshengliang@outlook.com>
2022-11-30 17:55:37 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import axios from "axios";
 function App() {
 
     useEffect(() => {
+
+        if (localStorage.getItem("result")) {
+            alert(localStorage.getItem("result"));
+
+            axios.post("https://ekyc-redcash.hardcore-carver.42-1-60-64.plesk.page/redCash/api/v1.0/wiseai/result", JSON.parse(localStorage.getItem("result")))
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    alert(err);
+                })
+
+        }
+
         axios.post('https://ekyc-redcash.hardcore-carver.42-1-60-64.plesk.page/redCash/api/v1.0/wiseai/token', {}, {
             headers: {
                 Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3aXNlYWkiLCJzdWIiOiJ3aXNlYWktYXBpIiwiaWQiOiIxNDkiLCJubSI6IldlYiBUZXN0aW5nIiwibWlzYyI6IiIsInZlciI6M30.QVJ-KsQ1G_G9Y2wXYaWPolhmJWJLni4GeQUjay8BIEw`
             }
         })
             .then((res) => {
-                localStorage.setItem('tokenKey', res.data.data.token);
+                localStorage.setItem('key', res.data.data.token);
+                localStorage.setItem('iv', res.data.data.encryption.iv);
             })
-    }, [])
+    }, [localStorage])
 
     return (
         <div className="App">
