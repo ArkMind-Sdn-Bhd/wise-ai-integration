367b7f3ac29c03d04320b8e0d9cd68f97e5b675c
draden <hanshengliang@outlook.com>
2022-12-01 14:21:20 +0800

    Ekyc

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
 
     useEffect(() => {
 
+        if (localStorage.getItem("result")) {
+
+            axios.post("https://ekyc-redcash.hardcore-carver.42-1-60-64.plesk.page/redCash/api/v1.0/wiseai/result", JSON.parse(localStorage.getItem("result")))
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    alert(err);
+                })
+
+        }
+
         axios.post('https://ekyc-redcash.hardcore-carver.42-1-60-64.plesk.page/redCash/api/v1.0/wiseai/token', {}, {
             headers: {
                 Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3aXNlYWkiLCJzdWIiOiJ3aXNlYWktYXBpIiwiaWQiOiIxNDkiLCJubSI6IldlYiBUZXN0aW5nIiwibWlzYyI6IiIsInZlciI6M30.QVJ-KsQ1G_G9Y2wXYaWPolhmJWJLni4GeQUjay8BIEw`
